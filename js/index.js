96698196e285efb099643144ac575a6696cc23b2
olkonah1121 <vowan-n@ukr.net>
2020-02-13 22:09:30 +0000

    Calculation

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
-const display = document.querySelector ('.display')
-document.querySelectorAll ('.digits button')
- .forEach( button => button.addEventListener('click', digitPressed));
- function digitPressed(ev) {
-     display.value += ev.target.innerText;
- }
+const display = document.querySelector(".display");
 
- document.querySelectorAll ('.operations button')
- .forEach( button => button.addEventListener('click', operationsPressed));
- function operationsPressed(ev) {
-     display.value += ev.target.innerText;
- }
- document.querySelector('.equal').addEventListener('click' ,calculate);
- function calculate() {
-     display.value = eval(display.value);
- }
+document
+  .querySelectorAll(".digits button")
+  .forEach(button => button.addEventListener("click", digitPressed));
+function digitPressed(ev) {
+  display.value += ev.target.innerText;
+}
 
+document
+  .querySelectorAll(".operations button")
+  .forEach(button => button.addEventListener("click", operationsPressed));
+function operationsPressed(ev) {
+  display.value += ev.target.innerText;
+}
+
+document.querySelector(".equal").addEventListener("click", calculate);
+function calculate() {
+  if (eval(display.value) === Infinity) display.value = "I can not do this";
+  else
+    display.value[display.value.length - 1] == "+" ||
+      display.value[display.value.length - 1] == "-" ||
+      display.value[display.value.length - 1] == "*" ||
+      display.value[display.value.length - 1] == "/" ||
+      display.value[display.value.length - 1] == ".";
+  display.value = eval(display.value);
+}
+
+document.querySelector(".clearall").addEventListener("click", clearResults);
+function clearResults() {
+  display.value = "";
+}
+
+document.querySelector(".clearone").addEventListener("click", deleteLast);
+function deleteLast() {
+  display.value = display.value.substring(0, display.value.length - 1);
+}
